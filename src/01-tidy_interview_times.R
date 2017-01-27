3848c32de226d5bd5e196e0515ec0d50fec6ef28
bgulbis <brian.gulbis@gmail.com>
2017-01-27 14:36:40 -0600

    save tidy data

--- src/01-tidy_interview_times.R
+++ src/01-tidy_interview_times.R
@@ -4,12 +4,14 @@ library(tidyverse)
 library(stringr)
 library(lubridate)
 
-raw_interviews <- read_csv("data/raw/interview_times.csv") %>%
+interview_times <- read_csv("data/raw/interview_times.csv") %>%
     rename(first_name = `First Name`,
            last_name = `Last Name`,
            email = Email) %>%
     extract(`Sign Up Items`,
-            c("interview_date", "interview_time"),
+            c("interview_date", "morning"),
             "(02/[0-9]{2}/2017) (8|11)") %>%
-    dmap_at("interview_date", mdy)
+    dmap_at("interview_date", mdy) %>%
+    dmap_at("morning", ~ .x == "8")
 
+write_rds(interview_times, "data/tidy/interview_times.Rds", "gz")
