4ccfb5da5c1599a85ccfd72eef30b4a4d667a90d
bgulbis <brian.gulbis@gmail.com>
2017-01-31 12:37:41 -0600

    add group assignments

--- src/01-tidy_interview_times.R
+++ src/01-tidy_interview_times.R
@@ -9,9 +9,11 @@ interview_times <- read_csv("data/raw/interview_times.csv") %>%
            last_name = `Last Name`,
            email = Email) %>%
     extract(`Sign Up Items`,
-            c("interview_date", "morning"),
+            c("interview_date", "pm"),
             "(02/[0-9]{2}/2017) (8|11)") %>%
     dmap_at("interview_date", mdy) %>%
-    dmap_at("morning", ~ .x == "8")
+    dmap_at("pm", ~ .x == "11") %>%
+    group_by(interview_date, pm) %>%
+    mutate(assignment = seq(n()))
 
 write_rds(interview_times, "data/tidy/interview_times.Rds", "gz")
